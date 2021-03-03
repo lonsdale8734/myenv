a7d340960168c11c1a4e05e02c4cf56eae946481
lons <lonsdale8734@gmail.com>
2021-03-03 21:56:42 +0800

    update myenv.sh

--- .myenv/myenv.sh
+++ .myenv/myenv.sh
@@ -4,6 +4,16 @@ source "$MYENV/antigen.sh"
 source "$MYENV/pyenv.sh"
 source "$MYENV/go.sh"
 
+# set PATH so it includes user's private bin if it exists
+if [ -d "$HOME/bin" ] ; then
+    PATH="$HOME/bin:$PATH"
+fi
+
+# set PATH so it includes user's private bin if it exists
+if [ -d "$HOME/.local/bin" ] ; then
+    PATH="$HOME/.local/bin:$PATH"
+fi
+
 # User configuration
 alias myenv='/usr/bin/git --git-dir=$HOME/github/myenv/ --work-tree=$HOME'
 alias vi="vim"
