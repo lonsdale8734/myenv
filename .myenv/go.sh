bf634da994aff4c10f0e0eb3748477c05c82e5d8
lons <lonsdale8734@gmail.com>
2022-06-10 11:36:20 +0800

    update go.sh pyenv.sh

--- .myenv/go.sh
+++ .myenv/go.sh
@@ -1,4 +1,6 @@
 if [ -d "$HOME/.local/go/bin" ] ; then
     PATH="$HOME/.local/go/bin:$PATH"
     export GOPATH="$HOME/.local/go"
+    export GO111MODULE=on
+    export GOPROXY="https://goproxy.io,direct"
 fi
